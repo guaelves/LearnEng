f3015ceb06815ee427422fbb3a972766c355a9c8
guaelves <yutake1016@gmail.com>
2026-01-09 14:14:16 +0800

    Update sw.js

--- 220SW/sw.js
+++ 220SW/sw.js
@@ -1,18 +1,7 @@
-const CACHE_NAME = 'eng-v2';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
